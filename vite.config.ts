ba3b1f278f3968ff953988eb934610a3d8d2433c
Federica <federica.correa@humand.co>
2026-08-27 15:55:51 -0300

    Remove material-hu

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { getNodeModule } from 'material-hu/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -20,21 +19,6 @@ export default defineConfig({
           xlsx: 'XLSX',
         },
       },
-    },
-  },
-  resolve: {
-    alias: {
-      '@material-hu/mui/lab': getNodeModule('@mui/lab'),
-      '@material-hu/mui/x-date-pickers': getNodeModule('@mui/x-date-pickers'),
-      '@material-hu/mui': getNodeModule('@mui/material'),
-      '@material-hu/icons/material': getNodeModule('@mui/icons-material'),
-      '@material-hu/icons/tabler': getNodeModule('@tabler/icons-react'),
-      '@material-hu/hooks': getNodeModule('material-hu/lib/hooks'),
-      '@material-hu/utils': getNodeModule('material-hu/lib/utils'),
-      '@material-hu/types': getNodeModule('material-hu/lib/types'),
-      '@material-hu/styles': getNodeModule('material-hu/lib/styles'),
-      '@material-hu/theme': getNodeModule('material-hu/lib/theme'),
-      '@material-hu/components': getNodeModule('material-hu/lib/components'),
     },
   },
 });
